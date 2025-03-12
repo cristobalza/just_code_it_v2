030b54f0f63bc36594a1450f45ced0682b57a04c
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-03-11 20:18:22 -0500

    Time: 6 ms (84.7%), Space: 17.9 MB (95.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        new_s = ""
+        for char in s:
+            if char.isalnum():
+                new_s += char
+        
+        return new_s == new_s[::-1]
