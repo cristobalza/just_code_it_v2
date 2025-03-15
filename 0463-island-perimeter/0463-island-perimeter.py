eb17e711638d1db986aa48496e50122ae3291d0f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-03-15 00:21:41 -0500

    Time: 158 ms (16.8%), Space: 27.9 MB (5.24%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,40 +1,33 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        # dfs
+        m, n = len(grid), len(grid[0])
+
+        res = 0
 
         visited = set()
-        m, n = len(grid), len(grid[0])
-        perimeter = 0
 
-        def dfs(row, col):
-            nonlocal visited, m, n, perimeter
+        def dfs(r, c):
+            nonlocal m, n, res, visited
 
-            if (row, col) not in visited and 0 <= row < m and 0 <= col < n:
-                visited.add((row, col))
-                # Check north
-                if (row - 1 >= 0 and grid[row - 1][col] == 0) or row - 1 < 0:
-                    perimeter += 1
-                # Check south
-                if (row + 1 < m and grid[row + 1][col] == 0) or row + 1 == m:
-                    perimeter += 1
-                # Check west
-                if (col - 1 >= 0 and grid[row][col - 1] == 0) or col - 1 < 0:
-                    perimeter += 1
-                # Check east
-                if (col + 1 < n and grid[row][col + 1] == 0) or col + 1 == n:
-                    perimeter += 1
+            if (r, c) in visited:
+                return
+
+            if r < 0 or r == m or c < 0 or c == n or (0 <= r < m and grid[r][c] == 0) or (0 <= c < c and grid[r][c] == 0):
+                res += 1
+                return 
+
+            visited.add((r, c))
             
-            # Explore
-            explore_list = [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]
-            for next_row, next_col in explore_list:
-                if 0 <= next_row < m and 0 <= next_col < n and grid[next_row][next_col] == 1 and (next_row, next_col) not in visited:
-                    dfs(next_row, next_col)
-        
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1:
-                    dfs(row, col)
-        return perimeter
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
 
+            return 
 
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 1:
+                    dfs(r, c)
+        return res
         
