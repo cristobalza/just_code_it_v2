e1d55cc68e1ddd806d6f276a1e97b105eb6025a9
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-04-17 21:38:34 -0500

    Time: 0 ms (100%), Space: 19.1 MB (24.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        subtrahend_dict = {}
+        hm = {}
+
         for i, num in enumerate(nums):
-            if target - num in subtrahend_dict:
-                return [i, subtrahend_dict[target - num]]
-            else:
-                subtrahend_dict[num] = i
+            if target - num in hm:
+                return [hm[target - num], i]
+            hm[num] = i
+            
+        
