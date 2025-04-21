8b2753d991d955d0b478f67d92950e2e91b87fee
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-04-20 22:41:13 -0500

    Time: 0 ms (100%), Space: 17.9 MB (18.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -5,14 +5,14 @@ class Solution:
 
         area = m * n
 
-        upper_limit, bottom_limit, left_limit, right_limit = 0, m-1, 0, n-1
+        upper_limit, bottom_limit, left_limit, right_limit = 0, m, 0, n
 
         res = []
 
         while area > 0: 
 
             # To Right
-            for col in range(left_limit, right_limit + 1):
+            for col in range(left_limit, right_limit):
                 area -= 1
                 res.append(matrix[upper_limit][col])
             upper_limit += 1
@@ -20,30 +20,25 @@ class Solution:
                 break
 
             # To Down
-            for row in range(upper_limit, bottom_limit + 1):
-                print((row, right_limit))
+            for row in range(upper_limit, bottom_limit):
                 area -= 1
-                res.append(matrix[row][right_limit])
+                res.append(matrix[row][right_limit - 1])
             right_limit -= 1
             if area == 0:
                 break
 
             # To Left
-            for col in range(right_limit, left_limit - 1 , -1):
+            for col in range(right_limit - 1, left_limit - 1 , -1):
                 area -= 1
-                res.append(matrix[bottom_limit][col])
+                res.append(matrix[bottom_limit - 1][col])
             bottom_limit -= 1
             if area == 0:
                 break
 
             # To Up
-            for row in range(bottom_limit, upper_limit - 1, -1):
+            for row in range(bottom_limit - 1, upper_limit - 1, -1):
                 area -= 1
                 res.append(matrix[row][left_limit])
             left_limit += 1
         
         return res
-
-
-
-        
