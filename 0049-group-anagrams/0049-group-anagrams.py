b4ede9dd8e4b818de2c12d562a6caa95ce07de00
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-04-23 23:57:39 -0500

    Time: 19 ms (37.05%), Space: 22.6 MB (25.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict_map = {}
+        
+        hmap = collections.defaultdict(list)
 
         for word in strs:
-            mask_count_list = [0]*26
+            mask_arr = [0] * 26
+            for ch in word:
+                mask_arr[ord(ch) - ord("a")] += 1
 
-            for char in word:
-                mask_count_list[ord(char) - ord("a")] += 1
-            
-            # Save
-            if tuple(mask_count_list) in dict_map:
-                dict_map[tuple(mask_count_list)].append(word)
-            else:
-                dict_map[tuple(mask_count_list)] = []
-                dict_map[tuple(mask_count_list)].append(word)
+            hmap[tuple(mask_arr)].append(word)
         
-        return list(dict_map.values())
+        return list(hmap.values())
+
