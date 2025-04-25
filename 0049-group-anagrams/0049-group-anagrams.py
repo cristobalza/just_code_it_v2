10933b1feb7cbb84ec0c76ad5682bc1de21e6cf7
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-04-25 12:44:10 -0500

    Time: 20 ms (26.36%), Space: 22.7 MB (25.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         hmap = collections.defaultdict(list)
-
         for word in strs:
             mask_arr = [0] * 26
             for ch in word:
                 mask_arr[ord(ch) - ord("a")] += 1
-
             hmap[tuple(mask_arr)].append(word)
-        
         return list(hmap.values())
-
