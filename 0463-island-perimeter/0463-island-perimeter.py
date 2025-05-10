2fad843b5ce9b240f42d1b5594cd7c974839f25b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-05-09 21:18:37 -0500

    Time: 215 ms (6.92%), Space: 21.6 MB (18.47%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,33 +1,34 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-
-        res = 0
-
         visited = set()
 
-        def dfs(r, c):
-            nonlocal m, n, res, visited
+        ROWS, COLS = len(grid), len(grid[0])
+
+        perimeter = 0
+
+        def dfs(r, c, visited):
+            nonlocal perimeter, ROWS, COLS
 
             if (r, c) in visited:
-                return
-
-            if r < 0 or r == m or c < 0 or c == n or (0 <= r < m and grid[r][c] == 0) or (0 <= c < c and grid[r][c] == 0):
-                res += 1
                 return 
 
-            visited.add((r, c))
+            if r == ROWS or r < 0 or c == COLS or c < 0 or grid[r][c] == 0:
+                perimeter += 1
+                return
             
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+            visited.add((r, c))
+
+            for _r, _c in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
+                dfs(_r, _c, visited)
 
             return 
-
-        for r in range(m):
-            for c in range(n):
+        
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == 1:
-                    dfs(r, c)
-        return res
+                    dfs(r, c, visited)
+                    
+        return perimeter
+
+            
         
