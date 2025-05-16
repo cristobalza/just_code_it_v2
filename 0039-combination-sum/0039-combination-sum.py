49ece3e7189c532ec7ce60ccdb34260be2fcf215
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-05-16 17:06:35 -0500

    Time: 95 ms (7.08%), Space: 18 MB (19.74%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def backtrack(i, subset):
+            nonlocal target, res, candidates
+
+            if subset and sum(subset) == target:
+                res.add(tuple(subset[:]))
+                return 
+
+            if subset and sum(subset) > target:
+                return
+
+            for j in range(i, len(candidates)):
+                subset.append(candidates[i])
+                backtrack(j, subset)
+                subset.pop()
+            
+            return 
+
+        res = set()
+
+        for i in range(len(candidates)):
+            backtrack(i, [])
+
+        return list(res)
+        
