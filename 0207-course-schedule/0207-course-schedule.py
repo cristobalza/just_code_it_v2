330a3de859c761f4ec4eec59cb933fa1a1f7a4fb
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-05-22 23:10:51 -0500

    Time: 7 ms (47.73%), Space: 19.7 MB (14.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,35 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        def dfs(course, course_cycle_set):
-            nonlocal course_visit_set, graph
+        def backtrack(course, cycle_set):
+            nonlocal visited
 
-            if course in course_cycle_set:
-                return False
-
-            if course in course_visit_set:
+            if course in visited:
                 return True
 
-            course_cycle_set.add(course)
+            if course in cycle_set:
+                return False
+
+            cycle_set.add(course)
 
             for next_course in graph[course]:
-                if not dfs(next_course, course_cycle_set):
+                if not backtrack(next_course, cycle_set):
                     return False
             
-            course_cycle_set.remove(course)
-            course_visit_set.add(course)
+            cycle_set.remove(course)
+            visited.add(course)
 
             return True
 
-        course_visit_set = set()
-
-        graph = {course_num : [] for course_num in range(numCourses)}
+        graph = {node: [] for node in range(numCourses)}
 
         for a, b in prerequisites:
             graph[a].append(b)
 
+        visited = set()
+
         for course in range(numCourses):
-            if not dfs(course, set()):
+            if not backtrack(course, set()):
                 return False
-        return True
         
+        return True
