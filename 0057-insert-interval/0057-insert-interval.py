29d8a263319a7a5c4b51382e9af7a5e9c5aaf10f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-06-07 23:57:17 -0500

    Time: 5 ms (10.04%), Space: 19.7 MB (60.89%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,20 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # case 1 you insert in between two intervals
-        # case 2 you insert and merge one of them 
-        # case 3 the new interval merges multiple intervals
-        # case 4 the new interval is inserted at the begining or at the end
+        # case 1 you insert the newInterval and append the rest of the intervals
+        # case 2 
+
 
         stack = []
 
         for i, interval in enumerate(intervals):
 
-            if interval[1] < newInterval[0]:
-                stack.append(interval)
-
-            elif newInterval[1] < interval[0]:
+            if newInterval[1] < interval[0]:
                 stack.append(newInterval)
                 return stack + intervals[i:]
+
+            elif interval[1] < newInterval[0]:
+                stack.append(interval)
 
             else:
                 newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
