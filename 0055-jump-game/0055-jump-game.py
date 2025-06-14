7dece542b166f4cc39d2e8f72dc083ab1149079f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-06-14 17:13:20 -0500

    Time: 10706 ms (5%), Space: 50.9 MB (5.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,24 +1,29 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-    
+        
         def helper(i):
+            
             if i in memo:
                 return memo[i]
 
-            if i == len(nums) - 1:
+            if i >= (n - 1):
+                memo[i] = True
                 return True
 
             if nums[i] == 0:
+                memo[i] = False
                 return False
 
-            end = min(len(nums), nums[i] + i + 1)
-            
-            for j in range(i + 1, end):
+            jump = min(i + nums[i], n - 1) # keep in bound for the iteration
+
+            for j in range(i + 1, jump + 1):
                 if helper(j):
                     memo[i] = True
                     return True
+
             memo[i] = False
             return False
             
+        n = len(nums)
         memo = {}
         return helper(0)
