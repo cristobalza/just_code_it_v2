3a486ad1d564d0810c567b11d522be49eece704c
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-06-15 18:45:16 -0500

    Time: 14 ms (84.79%), Space: 17.7 MB (82.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,18 +4,17 @@ class Solution:
             return 0
         
         res = 0
-        hmap = {}
+        hmap = collections.defaultdict(int)
         l = 0
         
         for r in range(len(s)):
-            # If character is already in window, shrink from left
-            if s[r] in hmap and hmap[s[r]] >= l:
+
+            if s[r] in hmap and hmap.get(s[r], 0) >= l:
                 l = hmap[s[r]] + 1
             
-            # Update the character's position
+
             hmap[s[r]] = r
             
-            # Update result with current window size
             res = max(res, r - l + 1)
             
         return res
