b61337701805370eaa74e27195628775762344f5
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-06-23 16:42:41 -0500

    Time: 154 ms (38.04%), Space: 18 MB (48.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,25 +1,40 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-        l = 0
-        max_freq = 0
+        """
+
+        s = "AABABBA", k = 1
+                  r
+               l
+
+        lenght of word - max count <= k
         
+        5             -  3      ==> 2 <= 1 
+
+        hmap = {A: 2, B: 3}
+
+
+
+
+        """
+
+        if len(s) == 0:
+            return 0
+
+        res = 1
+        l = 0
+        hmap = collections.defaultdict(int)
+
         for r in range(len(s)):
-            # Add current character to window
-            count[s[r]] = count.get(s[r], 0) + 1
-            
-            # Update max frequency in current window
-            max_freq = max(max_freq, count[s[r]])
-            
-            # If window is invalid (need more than k replacements)
-            window_size = r - l + 1
-            if window_size - max_freq > k:
-                # Shrink window from left
-                count[s[l]] -= 1
+
+            hmap[s[r]] = hmap.get(s[r], 0) + 1
+
+            lenght_word = r - l + 1
+            max_count = max(hmap.values())
+
+            if lenght_word - max_count > k:
+                hmap[s[l]] = hmap.get(s[l]) - 1
                 l += 1
-            
-            # Update result
+
             res = max(res, r - l + 1)
-            
+
         return res
