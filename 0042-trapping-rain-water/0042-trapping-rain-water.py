8c38530afa956ac3c5264d5809219ba00c9eecbb
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-06-28 23:27:24 -0500

    Time: 13 ms (56.84%), Space: 19.2 MB (68.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,10 +3,9 @@ class Solution:
 
         res = 0
 
-        l = 0
-        r = len(height) - 1
+        max_l, max_r = height[0], height[len(height) - 1]
 
-        max_l, max_r = height[l], height[r]
+        l, r = 0, len(height) - 1
 
         while l < r:
 
@@ -14,14 +13,10 @@ class Solution:
                 l += 1
                 max_l = max(max_l, height[l])
                 res += max_l - height[l]
-
             else:
                 r -= 1
                 max_r = max(max_r, height[r])
                 res += max_r - height[r]
 
         return res
-            
-
-
         
