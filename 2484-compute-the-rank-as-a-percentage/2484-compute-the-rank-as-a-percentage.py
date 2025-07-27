78b9e6922af9b8b3e45d1109d1144b029c0d96be
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-07-27 16:59:15 -0500

    Time: 392 ms (75%), Space: 68.7 MB (62.5%) - LeetHub

---
+++ 2484-compute-the-rank-as-a-percentage/2484-compute-the-rank-as-a-percentage.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def compute_rating(students: pd.DataFrame) -> pd.DataFrame:
+    df = students.copy()
+    
+    df["rank"] = df.groupby('department_id')['mark'].rank(method='min', ascending=False)
+    
+    df["count"] = df.groupby('department_id')['student_id'].transform('count')
+    
+    df['percentage']=np.where(
+        df['count']>1,
+        round((df['rank']-1)*100/(df['count']-1),2),0.0
+    )
+    
+    result = df[["student_id", "department_id", "percentage"]].sort_values(
+        ["department_id", "student_id"], ascending=[True, True]
+    )
+    
+    return result
