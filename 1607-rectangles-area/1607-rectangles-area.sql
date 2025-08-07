d04d96a7bc3b8870eece7da0adba172eb1aeee5d
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-06 22:48:55 -0500

    Time: 618 ms (5.14%), Space: 0B (100%) - LeetHub

--- 1607-rectangles-area/1607-rectangles-area.sql
+++ 1607-rectangles-area/1607-rectangles-area.sql
@@ -1,13 +1,20 @@
 # Write your MySQL query statement below
-SELECT 
-    p1.id AS p1, 
-    p2.id AS p2, 
-    ABS(p1.x_value - p2.x_value) * ABS(p1.y_value - p2.y_value) AS area
-FROM Points AS p1 
-INNER JOIN Points AS p2 
-    ON p1.id < p2.id
+WITH area_tbl AS (
+    SELECT
+        tbl1.id AS p1,
+        tbl2.id AS p2, 
+        ABS(tbl1.x_value - tbl2.x_value) * ABS(tbl1.y_value - tbl2.y_value) AS area
+    FROM Points AS tbl1
+    CROSS JOIN Points AS tbl2
+)
+
+SELECT
+    p1,
+    p2,
+    area
+FROM area_tbl
 WHERE 
-    p1.x_value != p2.x_value 
-    AND p1.y_value != p2.y_value
-ORDER BY area DESC, p1, p2
-;
+    area != 0
+    AND p1 < p2
+ORDER BY
+    area DESC, p1, p2
