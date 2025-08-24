07b80c44c14ced304c90fc93e9068384b4d3b7f0
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-24 18:49:02 -0500

    Time: 87 ms (34.47%), Space: 35.3 MB (89.1%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -19,20 +19,44 @@ class Solution:
 
         store in a list as iterate through the ll
         then iterate through the list to calculate the sum of the node + twin and store in a variable
+        O(N) time
+        O(N) space
+
+                0 1  | 2 3
+        head = [5,4, | 2,1]
+                <-     ->)
+
+        n = 4; n // 2 = 4 // 2 = 2
+
+        res = max(res, prev.val + forward.val)
+
+        slow and fast -> use the slow pointer to get the middle (forward)
+        as we iterate through it, change pointer direction to backwards
+
+        head = [5,4,2,1]
+                S
+                    F
+                  T
+              P
+                
+                  
+
         """
 
-        curr = head
-        n = 0
-        node_list = []
-        while curr:
-            n += 1
-            node_list.append(curr.val)
-            curr = curr.next
+        # slow fast
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
         
         res = 0
-        for i in range(n):
-            node_val = node_list[i]
-            twin_node_val = node_list[n - 1 - i]
-            res = max(res, node_val + twin_node_val)
+        while prev and slow:
+            res = max(res, prev.val + slow.val)
+            prev = prev.next
+            slow = slow.next
 
         return res
