18296189af8744fb5ac341356c7c719a86cc71b3
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-24 21:27:40 -0500

    Time: 6 ms (94.7%), Space: 28.3 MB (80.2%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,25 +1,34 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         """
-        target = 7, nums = [2,3,1,2,4,3]
-                              l
-                                    r
+        nums := List[int]
+        - positive integes
 
+        target:= int
+        - positive int
 
-                              6 >= 7
-        res = min(res, r - l + 1)
+        R: minimal lenght of a subarray whose sum >= target
+
+        Input: target = 7, nums = [2,3,1,2,4,3]
+                                           l
+                                               r
+
+                                     4 + 3 >= target = 7
+
+                                    res = 2
+
+        sliding window
         """
 
-        res = float("inf")
         l = 0
-        window_sum = 0
-
+        res = float("inf")
+        curr = 0
         for r in range(len(nums)):
-            window_sum += nums[r]
+            curr += nums[r]
 
-            while l <= r and window_sum >= target:
+            while l <= r and curr >= target:
                 res = min(res, r - l + 1)
-                window_sum -= nums[l]
+                curr -= nums[l]
                 l += 1
-
+            
         return res if res != float("inf") else 0
