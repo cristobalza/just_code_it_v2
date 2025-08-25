180bb2b32143d58d1652a5f0593aa07424d84219
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-25 15:26:45 -0500

    Time: 39 ms (16.29%), Space: 27.1 MB (5.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,30 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
+        n = len(nums)
+        prefix = [1] * n
+        prefix[0] = nums[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i]
 
-                  [1, 2, 3, 4]
+        postfix = [1] * n
+        postfix[-1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            postfix[i] = postfix[i + 1] * nums[i]
 
-    forward_arr   [1, 2, 6, 24]
-    backward_arr  [24, 24, 12, 4]
+        
+        answer = []
+        
+        for i in range(n):
+            prefix_val = prefix[i - 1] if i - 1 >= 0 else 1
+            postfix_val = postfix[i + 1] if i + 1 < n else 1
 
-
-    out [24, 12, 8, 6]
-
-
-        """
-
-        res = [1] * len(nums)
-        forward_arr = [1] * len(nums)
-        backward_arr = [1] * len(nums)
-
-        for i in range(1, len(nums)):
-            forward_arr[i] = nums[i - 1] * forward_arr[i - 1]
-
-        for i in range(len(nums) - 2, -1, -1):
-            backward_arr[i] = nums[i + 1] * backward_arr[i + 1]
-
-        for i in range(len(nums)):
-            res[i] = backward_arr[i] * forward_arr[i]
-
-        return res
+            answer.append(prefix_val * postfix_val)
+        
+        return answer
         
