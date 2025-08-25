1947f7707cb8bfac636ff9b5ce20bbbca04c657a
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-25 12:12:17 -0500

    Time: 2392 ms (5.03%), Space: 21.6 MB (86.81%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -4,7 +4,10 @@ class NumArray:
         self.nums = nums
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left: right+1])
+        res = 0
+        for i in range(left, right+1):
+            res += (self.nums[i])
+        return res
         
 
 
