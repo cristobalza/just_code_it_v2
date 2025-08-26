d0cca7a7c29967da85015dab1e5b9fc06de6bfbe
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-26 10:22:17 -0500

    Time: 19 ms (53.38%), Space: 17.7 MB (81.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,38 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        """  
-
-             01234567
-        s = "abcabcbb"
-              l
-                r
-
-        {
-            a: 0
-            b: 1
-            c: 2
-        }
-
-        3 >= 0
-
-        res = 1
-
-        """
-
         if len(s) == 0:
             return 0
-
-        res = 1
+        
         l = 0
+        res = 1
         hmap = {}
 
         for r in range(len(s)):
-
-            if s[r] in hmap and hmap[s[r]] >= l:
-                l = hmap[s[r]] + 1
+            if s[r] in hmap:
+                l = max(hmap[s[r]] + 1, l)
 
             hmap[s[r]] = r
 
             res = max(res, r - l + 1)
 
         return res
+
+        
