aee296e5e7583fce26e701beec4ff1a19b35ded9
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-27 17:21:05 -0500

    Time: 296 ms (14.91%), Space: 28.1 MB (12.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        def dfs(r, c):
-            nonlocal visited
-
-            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visited or grid[r][c] == "0":
-                return
-
+        
+        def dfs(r, c, visited):
+            if check_lambda(r, c):
+                return 
+            
             visited.add((r, c))
 
-            for _r, _c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                dfs(_r, _c)
-            
+            for _r, _c in [(r + 1, c), (r, c + 1), (r, c - 1), (r - 1, c)]:
+                if not check_lambda(_r, _c):
+                    dfs(_r, _c, visited)
+
             return 
 
-        ROWS, COLS = len(grid), len(grid[0])
+
+        res = 0
         visited = set()
-        count = 0
+        ROWS, COLS = len(grid), len(grid[0])
+
+        check_lambda = lambda r, c: r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or grid[r][c] == "0"
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    count += 1
-                    dfs(r, c)
-        return count
-        
+                if (r, c) not in visited and grid[r][c] == "1":
+                    res += 1
+                    dfs(r, c, visited)
 
-
-
-         
-        
-        
+        return res
