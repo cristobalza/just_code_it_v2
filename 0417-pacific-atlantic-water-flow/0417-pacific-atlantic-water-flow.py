7c26e74eafcc53f6849e9f1e06b8c94905667f73
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-27 18:57:14 -0500

    Time: 48 ms (24.38%), Space: 19.3 MB (65.17%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,38 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # Run dfs from atlantic (ROWS, 0) to pacific (0, COLS)
-
-        def dfs(r, c, visited, prev_h):
-            
-            if (r, c) in visited or r < 0 or c < 0 or r == ROWS or c == COLS:
-                return 
-
-            curr_h = heights[r][c]
-
-            if prev_h > curr_h:
+        
+        def dfs(r, c, prev_h, visited):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited:
                 return
             
+            curr_h = heights[r][c]
+
+            if curr_h < prev_h:
+                return 
+
             visited.add((r, c))
-        
-            for _r, _c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                dfs(_r, _c, visited, curr_h)
+
+            for _r, _c in [(r+1, c),(r-1, c),(r, c+1),(r, c-1)]:
+                dfs(_r, _c, curr_h, visited)
             
-            return
+            return 
 
-        
         ROWS, COLS = len(heights), len(heights[0])
-
-        atlantic_set, pacific_set = set(), set()
-
-        for r in range(ROWS):
-            dfs(r, 0, pacific_set, heights[r][0])
-            dfs(r, COLS - 1, atlantic_set, heights[r][COLS - 1])
-
-        for c in range(COLS):
-            dfs(0, c, pacific_set, heights[0][c])
-            dfs(ROWS - 1, c, atlantic_set, heights[ROWS - 1][c])
+        pacific_set, atlantic_set = set(), set()
 
         res = []
+
+        for c in range(COLS):
+            dfs(0, c, float("-inf"), pacific_set)
+            dfs(ROWS - 1, c, float("-inf"), atlantic_set)
+
+        for r in range(ROWS):
+            dfs(r, 0, float("-inf"), pacific_set)
+            dfs(r, COLS - 1, float("-inf"), atlantic_set)
 
         for r in range(ROWS):
             for c in range(COLS):
