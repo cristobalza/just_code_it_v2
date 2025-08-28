66a25515ec4e3da16dc8904e3093eadb753424ac
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-27 20:01:43 -0500

    Time: 7 ms (13.74%), Space: 19.8 MB (14.3%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,27 +1,24 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
 
-        def dfs(node, prev_node):
-            nonlocal visited
+        def dfs(curr, prev):
 
-            if node in visited:
+            if curr in cycle_set or curr == prev:
                 return False
 
-            visited.add(node)
+            cycle_set.add(curr)
 
-            for children in graph[node]:
-                if children != prev_node and not dfs(children, node):
+            for preq in graph[curr]:
+                if preq != prev and not dfs(preq, curr):
                     return False
             
             return True
 
+        cycle_set = set()
+
         graph = collections.defaultdict(list)
-
-        visited = set()
-
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
-        
-        return dfs(0, -1) and len(visited) == n
 
+        return dfs(0, -1) and len(cycle_set) == n 
