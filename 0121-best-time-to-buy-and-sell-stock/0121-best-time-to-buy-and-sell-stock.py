992f3e72088e81a20760b98a0000f304fddc2eaf
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-31 18:07:18 -0500

    Time: 107 ms (39.6%), Space: 26.8 MB (77.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        """
+
+        prices = [7,1,5,3,6,4]
+                    l
+                            r 
+
+                prices[r] - prices[l] = 
+
+        """
+
         l = 0
+        res = 0
 
         for r in range(len(prices)):
-            if prices[l] > prices[r]:
+            if prices[r] - prices[l] < 0:
                 l = r
-            
             res = max(res, prices[r] - prices[l])
 
         return res
