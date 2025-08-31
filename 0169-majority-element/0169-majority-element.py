9aef565c80008cb7aeebef1158373d1292afe510
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-31 18:54:19 -0500

    Time: 3 ms (82.35%), Space: 19.5 MB (18.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hmap = {}
-        max_freq = 0
+        res, count = 0, 0
 
         for num in nums:
-            hmap[num] = 1 + hmap.get(num, 0)
-            max_freq = max(max_freq, hmap[num])
-        
-        for num, freq in hmap.items():
-            if freq == max_freq:
-                return num
+            if count == 0:
+                res = num
+            
+            count += (1 if num == res else -1)
+
+        return res
