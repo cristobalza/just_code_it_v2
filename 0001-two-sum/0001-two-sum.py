9c39236b06fbd22d701128cd1013c9352700a4a4
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-08-31 14:17:23 -0500

    Time: 0 ms (100%), Space: 19 MB (23.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_idx_dict = {}
+        hmap = {}
 
         for i, num in enumerate(nums):
-            if target - num in num_idx_dict:
-                return [num_idx_dict[target - num], i]
-            
-            num_idx_dict[num] = i
-        
+            if target - num in hmap:
+                return [hmap.get(target - num), i] 
+
+            hmap[num] = i
         
