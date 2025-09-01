c349aec1e9e75143ecf1f43617cb94bc47aa932c
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-01 14:04:12 -0500

    Time: 20 ms (25.25%), Space: 22.6 MB (31.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Create hmap -> ascii array: list of words
+        """
+        use ord to get unicode number of a letter
+
+        build hashmap st tuple(arraya of unicode values) : list of corresponding words
+
+        [1,1,0,0,0] : bat
+
+        [1,0,0,0,0] : [nat, tan]
+
+        return the values of the hmap
+        """
+
         hmap = collections.defaultdict(list)
+
         for word in strs:
-            lst = [0] * 26
+            unicode_mask = [0]*26
+
             for ch in word:
-                lst[ord("a") - ord(ch)] += 1
-            hmap[tuple(lst)].append(word)
+                unicode_mask[ord(ch) - ord("a")] += 1
+
+            hmap[tuple(unicode_mask)].append(word)
+
         return list(hmap.values())
         
