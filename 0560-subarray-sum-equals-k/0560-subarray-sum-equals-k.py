113ca40efb693fa4615668ba3dce0e2236504272
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-02 10:02:26 -0500

    Time: 27 ms (83.56%), Space: 20.3 MB (71.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,31 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+                       0 1 2
+        Input: nums = [1,1,1], k = 2
+                           i
+
+        res = max(res, curr - k if found) = max(1, 1)
+        curr = 3 ; 3 - 2 = 1
+        hmap = { # prefixsum: count
+            0: 1
+            1: 1
+
+        }
+
+        """
+
+        hmap = {0: 1}
         res = 0
-        prefix_dict = {0:1}
-        curr = 0
+        curr_sum = 0
 
         for num in nums:
-            curr += num
-            diff = curr - k
+            curr_sum += num
 
-            res += prefix_dict.get(diff, 0)
+            diff = curr_sum - k
 
-            prefix_dict[curr] = 1 + prefix_dict.get(curr, 0)
-        
+            res += hmap.get(diff, 0)
+
+            hmap[curr_sum] = 1 + hmap.get(curr_sum, 0)
+
         return res
