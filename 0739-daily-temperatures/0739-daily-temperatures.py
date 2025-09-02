70c2eeda285e78c647f99cbfb67e7da2259d614b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-02 10:45:47 -0500

    Time: 131 ms (26.05%), Space: 33.3 MB (31.88%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,44 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        """
+        Input: temperatures = [73,74,75,71,69,72,76,73]
+                               i
+                                  j
+                                      k
+
+        use a stack
+        store values in stack
+        pop them if prev temp < curr temp
+        save in the prev idx the difference 
+        """
         n = len(temperatures)
+
         res = [0] * n
+
         stack = []
 
         for i, temp in enumerate(temperatures):
             while stack and stack[-1][0] < temp:
-                stack_temp, stack_idx = stack.pop()
-                res[stack_idx] = i - stack_idx
-            stack.append((temp, i))
-
-        return res
+                prev_temp, idx = stack.pop()
+                res[idx] = i - idx
             
+            stack.append((temp, i))
+        
+        return res
+
+
+
+
+
+        # res = [0] * n
+        # for i in range(n):
+        #     curr_temp = temperatures[i]
+
+        #     for j in range(i + 1, n):
+        #         next_temp = temperatures[j]
+        #         if next_temp - curr_temp > 0:
+        #             res[i] = j - i
+        #             break
+
+        # return res
+        
