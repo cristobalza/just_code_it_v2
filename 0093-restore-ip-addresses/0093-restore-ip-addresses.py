0c65cabfc069f998edd36574dc2e7ed51ba2e524
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-01 23:33:27 -0500

    Time: 4 ms (33.94%), Space: 17.9 MB (30.09%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        res = []
+
+        if len(s) > 12:
+            return res
+
+        def backtrack(i, dots, curr_ip):
+            if dots == 4 and i == len(s):
+                res.append(curr_ip[:-1])
+                return
+            
+            if dots > 4:
+                return 
+
+            for j in range(i, min(i + 3, len(s))):
+                if int(s[i: j+1]) <= 255 and (i == j or s[i] != "0"):
+                    backtrack(j + 1, dots + 1, curr_ip + s[i: j+1] + ".")
+
+        res = []
+        backtrack(0, 0, "")
+        return res
