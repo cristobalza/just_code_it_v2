f078b5c1ce4a46a401fa03565c15d997d9b759af
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-02 01:45:36 -0500

    Time: 300 ms (13.77%), Space: 28.2 MB (11.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(r, c, visited):
-            if check_lambda(r, c):
-                return 
+        def dfs(r, c):
+            if validate(r, c):
+                visited.add((r, c))
+
+                for _r, _c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                    dfs(_r, _c)
             
-            visited.add((r, c))
 
-            for _r, _c in [(r + 1, c), (r, c + 1), (r, c - 1), (r - 1, c)]:
-                if not check_lambda(_r, _c):
-                    dfs(_r, _c, visited)
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
 
-            return 
-
+        validate = lambda r, c: 0 <= r and 0 <= c and r < ROWS and c < COLS and grid[r][c] == "1" and (r, c) not in visited
 
         res = 0
-        visited = set()
-        ROWS, COLS = len(grid), len(grid[0])
-
-        check_lambda = lambda r, c: r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or grid[r][c] == "0"
 
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) not in visited and grid[r][c] == "1":
                     res += 1
-                    dfs(r, c, visited)
+                    dfs(r, c)
 
         return res
