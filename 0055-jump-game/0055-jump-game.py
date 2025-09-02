13bfa39b3e48fccd436e4573f11e53eb084e1bfb
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-01 22:20:24 -0500

    Time: 7554 ms (5%), Space: 18.4 MB (97.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,21 +1,20 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-
         n = len(nums)
 
         dp = [False] * n
-
         dp[0] = True
 
         for i in range(n):
 
             if dp[i] is False:
                 return False
- 
+
             jump = min(i + nums[i], n - 1)
 
             for j in range(i + 1, jump + 1):
                 dp[j] = True
-
+        
         return dp[-1]
+            
         
