24edef3a88a3dd75e26cdfa238fff4f569a7c94a
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-01 22:27:56 -0500

    Time: 3 ms (93.21%), Space: 20.9 MB (63.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,14 +6,14 @@ class Solution:
         stack.append(intervals[0])
 
         for i in range(1, len(intervals)):
-            prev_interval = stack[-1]
+            prev_interval = stack.pop()
             curr_interval = intervals[i]
 
             if prev_interval[1] >= curr_interval[0]:
                 new_interval = [min(prev_interval[0], curr_interval[0]), max(prev_interval[1], curr_interval[1])]
-                stack.pop()
                 stack.append(new_interval)
             else:
+                stack.append(prev_interval)
                 stack.append(curr_interval)
 
         return stack
