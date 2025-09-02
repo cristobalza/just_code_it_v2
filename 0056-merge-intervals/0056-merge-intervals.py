8dbefeae5a2eee6b53172b6b843935ea13f83d50
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-01 22:26:06 -0500

    Time: 8 ms (48.92%), Space: 20.9 MB (89.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        intervals.sort(key=lambda x : x[0])
+        intervals.sort(key=lambda x: x[0])
 
         stack = []
+        stack.append(intervals[0])
 
-        for i, interval in enumerate(intervals):
-            if i == 0:
-                stack.append(interval)
-            if stack[-1][1] < interval[0]:
-                stack.append(interval)
+        for i in range(1, len(intervals)):
+            prev_interval = stack[-1]
+            curr_interval = intervals[i]
+
+            if prev_interval[1] >= curr_interval[0]:
+                new_interval = [min(prev_interval[0], curr_interval[0]), max(prev_interval[1], curr_interval[1])]
+                stack.pop()
+                stack.append(new_interval)
             else:
-                start, end = stack.pop()
-                stack.append([start, max(end, interval[-1])])
-        
+                stack.append(curr_interval)
+
         return stack
-        
