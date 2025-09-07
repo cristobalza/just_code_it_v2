53923784abbb8d8fee45fee30d81db7f94cc6455
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-07 17:17:30 -0500

    Time: 0 ms (100%), Space: 18 MB (16.24%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,42 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        """
+
+        Input: numRows = 5
+
+        i = 0
+
+        [1]
+
+        1 = 1
+         0 1
+        [1 1]
+
+
+        i = 2
+         0 1 2
+        [1 2 1 ]
+
+
+        Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+        """
+        res = [] 
+        dp = [1]
+        res.append(dp)
+        n = numRows
+
+        for i in range(numRows - 1):
+            prev = res[-1]
+            dp = [1] * (len(prev) + 1)
+            for j in range((len(prev) + 1)):
+                if j == 0 or j == (len(prev)):
+                    continue
+                dp[j] = prev[j- 1] + prev[j]
+
+            res.append(dp)
+
+        return res
+
+        
