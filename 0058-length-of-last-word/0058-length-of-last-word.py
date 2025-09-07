148057ab7c452c7351a2647a269f6e86e44e8c3b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-07 16:38:14 -0500

    Time: 0 ms (100%), Space: 17.9 MB (17.72%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,7 +1,20 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
+        """
+        Input: s = "   fly me   to   the moon  "
+                                              i
 
-        s = s.split(" ")
+        """
+        # s = s.strip()
+        n = len(s) - 1
 
-        return len(s[-1])
+        while n >= 0 and s[n] == " ":
+            n -= 1
+        
+        # s = s.split(" ")
+        end = n
+        start = n
+        while start >= 0 and s[start].isalnum():
+            start -= 1
+
+        return end - start
