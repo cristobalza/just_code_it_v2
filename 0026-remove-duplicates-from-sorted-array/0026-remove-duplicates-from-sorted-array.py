348fddfed34d64165f30f632fb1ae84cdc78f812
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-07 16:23:21 -0500

    Time: 3 ms (46.63%), Space: 19 MB (48.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,22 +1,19 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         """
+        Input: nums = [0,1,2,1,1,2,2,3,3,4]
+                             l
+                                 r
 
-        nums = [1,2,2]
-                    l
-                      r
-    
-        return l
-
-        nums = [0,1,2,3,4,2,2,3,3,4]
-                          l.  
-                                  r 
-                
+            if nums[r - 1] !+ nums[r]:
+                nums[l]= nbums[r]
+                l++
         """
-        l = 1
+        l= 1
 
         for r in range(1, len(nums)):
             if nums[r - 1] != nums[r]:
                 nums[l] = nums[r]
                 l += 1
+
         return l
