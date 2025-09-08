b0e9ad091c4dbc40c3eb569273d157f718e5936b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-08 11:43:39 -0500

    Time: 0 ms (100%), Space: 17.7 MB (61.43%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,16 +1,6 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        
-        if n < 0:
+        if n <= 0:
             return False
 
-        x = 1
-
-        while x <= n: # 4^k <= n => log4(n) >= k
-        # Therefore, the number of iterations is proportional to log4(n), giving us O(log4(n)) time complexity.
-            if x == n:
-                return True
-
-            x *= 4
-
-        return False
+        return math.log(n) / math.log(4) % 1 == 0
