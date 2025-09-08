cf005bd9843035b93782d1677fa072d6877062d6
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-08 11:56:11 -0500

    Time: 0 ms (100%), Space: 17.7 MB (61.43%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n <= 0:
-            return False
+        #. A power of 4 is also a power of 2
+        # How to indetify it, if the n % 3 which is one less than 4, is 1
 
-        return math.log(n) / math.log(4) % 1 == 0
+        return n > 0 and (n & (n - 1)) == 0 and n % 3 == 1
