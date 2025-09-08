9af17a800af05394164ddba020d3532f2f1bfa4b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-08 11:37:19 -0500

    Time: 0 ms (100%), Space: 17.8 MB (61.43%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -6,7 +6,8 @@ class Solution:
 
         x = 1
 
-        while x <= n:
+        while x <= n: # 4^k <= n => log4(n) >= k
+        # Therefore, the number of iterations is proportional to log4(n), giving us O(log4(n)) time complexity.
             if x == n:
                 return True
 
