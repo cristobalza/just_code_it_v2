6316a8928752cbca6e81b342d5cdfc13e0e7ea0a
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-11 21:47:30 -0500

    Time: 0 ms (100%), Space: 18 MB (91.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,18 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-        Input: nums = [5,6,0,1,2,3,4], target = 0
-                       l           
-                                   r
-                             m
-        check if one is right or left side is sorted
-        then check if target is between that
-        if not, go to the opposite side and do the same 
-            since its rotated once, it will guarantee that at least one side is it (if exists)
-            else return -1
-        """
-
         n = len(nums)
+
         l, r = 0, n - 1
 
         while l <= r:
@@ -20,19 +9,15 @@ class Solution:
 
             if nums[m] == target:
                 return m
-            
-            # Check right side first
-            if nums[m] <= nums[r]:
-                if nums[m] < target and target <= nums[r]:
+
+            if nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
-            # If not, check left side
-            else:
-                if nums[l] <= target and target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1 
-
-        return -1
-        
+        return -1 
