76469b546abd079fec6c6b548eb009287bd37e0e
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-09-15 20:54:11 -0500

    Time: 195 ms (20.65%), Space: 31.7 MB (36.96%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
@@ -1,15 +1,8 @@
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
 
-        def gcm(a, b):
-            return math.gcd(a, b)
-
         def lcm(a, b):
-            if a == 0 or b == 0:
-                return 0  # LCM is 0 if either number is 0
             return abs(a * b) // math.gcd(a, b)
-
-        check_gcm = lambda a, b: gcm(a, b) > 1
         
         stack = []
 
@@ -17,7 +10,7 @@ class Solution:
 
             stack.append(num)
 
-            while len(stack) >= 2 and check_gcm(stack[-2], stack[-1]):
+            while len(stack) >= 2 and math.gcd(stack[-2], stack[-1]) > 1:
                 a = stack.pop()
                 b = stack.pop()
 
