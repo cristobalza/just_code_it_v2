9f2692ff0c6e45dacfe574105bf360896cd81ef7
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-07 22:29:58 -0600

    Time: 11 ms (72.02%), Space: 18.3 MB (56.39%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,58 +1,34 @@
-'''
-Given n : integer, R: a baord :2D of nxn size that shows all possible Q positiionion without inteferring one with anohter . the number Qs is the same as n
-
-n = 4
-  0 1 2 3
-[[. Q . . ]]
- [. . . Q ]
- [Q . . . ]
- [. . Q . ]]
-
-backtracking solution because it can show every sinlge position
-
-[[* Q * * ]]
- [* * * Q ]
- [Q * * * ]
- [* * Q * ]]
- 
- 
- n == 0 solution
- 
- mark candidate : baord[r][c] = Q , then do all marking with *
- 
-
-
-'''
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col_hset = set()
-        postive_diag_hset = set()
-        negative_diag_hset = set()
-        
-        
-        board = [['.'] * n for _ in range(n)]
+        col_set = set()
+        positive_diag_set = set()
+        negative_diag_set = set()
+
+        board = [
+            ["."] * n for _ in range(n)
+        ]
+
         res = []
-        
+
         def backtrack(r):
+
             if r == n:
-                temp = ["".join(row) for row in board]
-                res.append(temp)
+                res.append(["".join(board[row]) for row in range(n)])
                 return
             else:
                 for c in range(n):
-                    if c not in col_hset and (r + c) not in postive_diag_hset and (r - c) not in negative_diag_hset:
-                        col_hset.add(c)
-                        postive_diag_hset.add((r + c))
-                        negative_diag_hset.add((r - c))
-                        board[r][c] = 'Q'
+                    if c not in col_set and r + c not in positive_diag_set and r - c not in negative_diag_set:
+                        board[r][c] = "Q"
+                        col_set.add(c)
+                        positive_diag_set.add(r + c)
+                        negative_diag_set.add(r - c)
 
                         backtrack(r + 1)
 
-                        col_hset.remove(c)
-                        postive_diag_hset.remove((r + c))
-                        negative_diag_hset.remove((r - c))
-                        board[r][c] = '.'
+                        board[r][c] = "."
+                        col_set.remove(c)
+                        positive_diag_set.remove(r + c)
+                        negative_diag_set.remove(r - c)
+
         backtrack(0)
         return res
-                    
-                    
