09b0e6c9f3f0ac150840de7ea353ccf72ce697ff
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-08 12:58:24 -0600

    Time: 0 ms (100%), Space: 18.2 MB (16.16%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,40 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        """
-        Input: nums = [1,2,3]
-
-        all subsets have n size
-        no dups
-
-        backtrack approach
-
-        edg case: subset reach same size of nums
-
-        mark candidate: do a permutation with next value
-
-        explore: call recursively to i + 1
-
-        remove candidate: do the same permutation 
-
-
-        Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-
-        """
-
+        
         def backtrack(i, subset):
-            if i == len(subset):
-                res.append(subset.copy())
-                return
+            if i == len(nums):
+                res.append(nums[::])
+                return 
 
             for j in range(i, len(nums)):
-                subset[i], subset[j] = subset[j], subset[i]
 
-                backtrack(i+1, subset)
+                nums[i], nums[j] = nums[j], nums[i]
 
-                subset[i], subset[j] = subset[j], subset[i]
-            
-            return 
+                backtrack(i + 1, subset)
+
+                nums[i], nums[j] = nums[j], nums[i]
+
 
         res = []
+
         backtrack(0, nums)
+
         return res
