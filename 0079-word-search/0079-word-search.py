f742d00b43c4c7f0f92dcf537d826cf85d2eb403
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-08 17:40:03 -0600

    Time: 5687 ms (16.09%), Space: 17.8 MB (87.49%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def backtrack(r, c, i):
 
-        def backtrack(r, c, i, word, visited):
             if i == len(word):
                 return True
 
-            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visited or board[r][c] != word[i]:
-                return False
+            if 0 <= r < ROWS and 0 <= c < COLS and i < len(word) and (r, c) not in visited and board[r][c] == word[i]:
+                visited.add((r, c))
+                for _r, _c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                    if backtrack(_r, _c, i + 1):
+                        return True
+                visited.remove((r, c))
 
-            visited.add((r, c))
+            return False
 
-            res = (
-                backtrack(r + 1, c, i + 1, word, visited) or
-                backtrack(r - 1, c, i + 1, word, visited) or
-                backtrack(r, c + 1, i + 1, word, visited) or
-                backtrack(r, c - 1, i + 1, word, visited)
-            )
-
-            visited.remove((r, c))
-
-            return res
-
-        ROWS, COLS = len(board), len(board[0])
+        visited, ROWS, COLS = set(), len(board), len(board[0])
 
         for r in range(ROWS):
             for c in range(COLS):
-                if backtrack(r, c, 0, word, set()):
+                if backtrack(r, c, 0):
                     return True
+
         return False
-        
