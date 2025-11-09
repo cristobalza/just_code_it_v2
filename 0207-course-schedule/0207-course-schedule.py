871c77c59d57ea82f4fc74d8712fae9bb77164a3
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-09 10:35:15 -0600

    Time: 7 ms (46.71%), Space: 19.8 MB (12.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,36 +1,61 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        def backtrack(course, cycled):
-            nonlocal visited, graph
+        """
+        Given numCourses and prerequisites, return true if can finish the coursers
+
+        adj. List
+        0 []
+        1 [0]
+
+
+        check for 0
+        {0, 1}
+        -> [1]
+            X
+            check for 1 and 
+            True
+        {1, 0}
+
+        same size of numCourses
+
+        backtracing approach
+
+        base case: if it is in the cycle (False) or has been visited (True)
+        mark candidate: add to the cycle check
+        explore: iterate through the prerequisites
+        remove candidate: rm from cycle and add to the visited
+        
+        return True
+        """
+
+        def backtrack(course, cycle):
+
+            if course in cycle:
+                return False
 
             if course in visited:
                 return True
-
-            if course in cycled:
-                return False
-
-            cycled.add(course)
+            
+            cycle.add(course)
 
             for prereq in graph[course]:
-                if not backtrack(prereq, cycled):
+                if not backtrack(prereq, cycle):
                     return False
 
-            cycled.remove(course)
+            cycle.remove(course)
             visited.add(course)
 
             return True
 
-        
-        graph = collections.defaultdict(list)
+        graph = {course: [] for course in range(numCourses)}
         for a, b in prerequisites:
             graph[a].append(b)
 
         visited = set()
 
         for course in range(numCourses):
-            if course in graph:
-                if not backtrack(course, set()):
-                    return False
+            if not backtrack(course, set()):
+                return False
 
-        return True
+        return True if len(visited) == numCourses else False
