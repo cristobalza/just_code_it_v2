5745c80e8e3dd59b9d41b5aaad526b765bc4deae
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-13 20:51:31 -0600

    Time: 11 ms (72.33%), Space: 17.9 MB (48.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        def helper(word):
-            lst = [0] * 26
-            
-            for ch in word:
-                lst[ord(ch) - ord("a")] += 1
-                
-            return lst  
 
         if len(s) != len(t):
             return False
+
+        s_list, t_list = [0] * 26, [0] * 26
+
+        for i in range(len(s)):
+            s_list[ord(s[i]) - ord("a")] += 1
+            t_list[ord(t[i]) - ord("a")] += 1
+
+        return s_list == t_list
         
-        return helper(s) == helper(t)
