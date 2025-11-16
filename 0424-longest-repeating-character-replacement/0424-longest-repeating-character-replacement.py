bd77df7a4bb3f11e80e0618075b9db9ee8db80f8
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-16 11:11:48 -0600

    Time: 111 ms (84.05%), Space: 18.1 MB (49.04%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,39 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        """
-
-        Input: s = "ABAB", k = 2
-                    l
-                      r 
-
-        {
-            A: 2
-            B: 1
-        }
-        total - max_f > k
-            hmap[k] -= 1
-            l += 1
-
-        3 - 2 > k
-        """
-
-        l = 0
         hmap = {}
-        max_freq = 0
+        l = 0
         res = 0
+        max_count = 0
 
         for r in range(len(s)):
-            
             hmap[s[r]] = 1 + hmap.get(s[r], 0)
 
-            max_freq = max(max_freq, hmap[s[r]])
+            max_count = max(max_count, hmap[s[r]])
 
-            if (r - l + 1) - max_freq > k:
+            if (r - l + 1) - max_count > k:
                 hmap[s[l]] -= 1
                 l += 1
-            
+
             res = max(res, r - l + 1)
 
         return res
-
-        
