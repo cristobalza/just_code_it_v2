c7a852f6de6664e44428a8d20b3d11b10c44e503
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-20 20:50:48 -0600

    Time: 1151 ms (16.39%), Space: 46 MB (6.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,52 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        coins = [1,2,5], amount = 11
 
-        # Bottom up
 
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
+                        1
+                       1 2 5
+                       1 ...
+                       1  6x   5
+                       1       3x
+                       1
+                       ...
+                       11x
 
-        for i in range(1, amount + 1):
+        memoization:
+
+        base case: amount == 0: return 0 or if amount in memo: return memo
+
+        res = inf
+        for coin in coins
+            if  new_amount -> amount - coin >= 0
+                    res = min(res, 1 + recursion call dfs( new_amount))
+                                    add one more coin
+
+        memo[amount] = res
+        return memo[amount]
+
+        O(coins^amount) time 
+        O(amount) space
+        """
+
+        def dfs(amount):
+            if amount == 0:
+                return 0
+
+            if amount in memo:
+                return memo[amount]
+
+            res = float("inf")
             for coin in coins:
-                if i - coin >= 0:
-                    dp[i] = min(dp[i], 1 + dp[i - coin])
+                if amount - coin >= 0:
+                    res = min(res, 1 + dfs(amount - coin))
+            
+            memo[amount] = res
+            return memo[amount]
 
-        return dp[-1] if dp[-1] != float("inf") else -1
+        memo = {}
+
+        min_number_coins = dfs(amount)
+
+        return min_number_coins if min_number_coins != float("inf") else -1
