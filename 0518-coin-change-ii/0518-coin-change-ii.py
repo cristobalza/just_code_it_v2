216c36f2af2eb3a8a118743e599a41e0da1c8c3e
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-23 12:04:08 -0600

    Time: 1693 ms (9.25%), Space: 364.7 MB (14.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -3,24 +3,24 @@ class Solution:
 
         # Memoization
 
-        def dfs(i, curr_amount):
+        def dfs(i, amount):
 
             # Base case
-            if i == len(coins) or curr_amount > amount:
+            if i == len(coins) or amount < 0:
                 return 0
 
-            if curr_amount == amount:
+            if amount == 0:
                 return 1
 
-            if (i, curr_amount) in memo:
-                return memo[(i, curr_amount)]
+            if (i, amount) in memo:
+                return memo[(i, amount)]
 
             # Recursive call:
             #     1. Try current coin
             #     2. Skip current coin
-            memo[(i, curr_amount)] = dfs(i, curr_amount + coins[i]) + dfs(i + 1, curr_amount)
+            memo[(i, amount)] = dfs(i, amount - coins[i]) + dfs(i + 1, amount)
 
-            return memo[(i, curr_amount)]
+            return memo[(i, amount)]
 
         memo = {}
-        return dfs(0, 0)
+        return dfs(0, amount)
