b35da6c1c43e2fa06c7ea24c6abf838ba77d1068
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-23 13:56:38 -0600

    Time: 47 ms (20.9%), Space: 19.3 MB (75.26%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,38 +1,29 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
-        def dfs(r, c, prev_h, visited):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited:
-                return
-            
-            curr_h = heights[r][c]
+        def dfs(r, c, prev_height, visited):
+            if 0 <= r < ROWS and 0 <= c < COLS and (r, c) not in visited and heights[r][c] >= prev_height:
+                visited.add((r, c))
 
-            if curr_h < prev_h:
-                return 
-
-            visited.add((r, c))
-
-            for _r, _c in [(r+1, c),(r-1, c),(r, c+1),(r, c-1)]:
-                dfs(_r, _c, curr_h, visited)
-            
-            return 
+                for _r, _c in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    dfs(r + _r, c + _c, heights[r][c], visited)
+            return
 
         ROWS, COLS = len(heights), len(heights[0])
+
         pacific_set, atlantic_set = set(), set()
-
-        res = []
-
-        for c in range(COLS):
-            dfs(0, c, float("-inf"), pacific_set)
-            dfs(ROWS - 1, c, float("-inf"), atlantic_set)
 
         for r in range(ROWS):
             dfs(r, 0, float("-inf"), pacific_set)
-            dfs(r, COLS - 1, float("-inf"), atlantic_set)
+            dfs(r, COLS-1, float("-inf"), atlantic_set)
 
+        for c in range(COLS):
+            dfs(0, c, float('-inf'), pacific_set)
+            dfs(ROWS-1, c, float("-inf"), atlantic_set)
+
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) in pacific_set and (r, c) in atlantic_set:
                     res.append((r, c))
-
         return res
