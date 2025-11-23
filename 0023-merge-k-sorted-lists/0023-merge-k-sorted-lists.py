26bfc5b367abf32eac78cd1654ee60d61d463c61
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-23 17:54:25 -0600

    Time: 1190 ms (9.58%), Space: 20 MB (97.8%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,39 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
+        def merge_linked_lists(l1, l2):
+            dummy = ListNode(-1)
+            curr = dummy
 
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next, l1 = l1, l1.next
+                else:
+                    curr.next, l2 = l2, l2.next
+
+                curr = curr.next
+
+            if l1:
+                curr.next = l1
+            else:
+                curr.next = l2
+            
+            return dummy.next
+
+
+        if not lists:
+            return None
+            
         curr = lists[0]
-        dummy = ListNode(-1, curr)
 
         for i in range(1, len(lists)):
-            new_linkedlist = lists[i]
-            curr = self.mergeTwoLinkedLists(curr, new_linkedlist)
-        
+            nxt = lists[i]
+
+            curr = merge_linked_lists(curr, nxt)
+
         return curr
-
-
-        
-    def mergeTwoLinkedLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
-        dummy = ListNode(-1)
-        curr = dummy
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                new_node = ListNode(l1.val)
-                l1 = l1.next
-            else:
-                new_node = ListNode(l2.val)
-                l2 = l2.next
-            
-            curr.next = new_node
-            curr = curr.next
-        
-        if l1:
-            curr.next = l1
-        else:
-            curr.next = l2
-
-        return dummy.next
