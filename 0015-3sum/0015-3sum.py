faca9178b987b32d441c790bf9be7dd35c6a01f1
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-26 12:55:36 -0600

    Time: 587 ms (51.11%), Space: 20.7 MB (71.07%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        n = len(nums)
+        res = []
 
-        for l in range(n):
-            
-            if 0 < l and nums[l - 1] == nums[l]:
+        for l in range(len(nums)):
+            # Skip the adjacent values
+            if l > 0 and nums[l - 1] == nums[l]:
                 continue
-
+            
             m = l + 1
-            r = n - 1
+            r = len(nums) - 1
 
             while m < r:
-                curr_sum = nums[l] + nums[m] + nums[r]
+                curr = nums[l] + nums[m] + nums[r]
 
-                if curr_sum == 0:
-                    res.append([nums[l], nums[m], nums[r]])
-                    m = m + 1
-                    while m < n and m < r and nums[m - 1] == nums[m]:
-                        m = m + 1
-
-                elif curr_sum > 0:
-                    r = r - 1
-
+                if curr < 0:
+                    m += 1
+                elif curr > 0:
+                    r -= 1
                 else:
-                    m = m + 1
+                    res.append([nums[l], nums[m], nums[r]])
+                    m += 1
+                    
+                    # Skip the adjacent values
+                    while m < r and m < len(nums) and nums[m - 1] == nums[m]:
+                        m += 1
 
-        return res
-        
+
+        return res                    
