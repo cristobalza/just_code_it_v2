2c558a8e51ea308e76f5510c43a4546bfde2774f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-27 16:42:03 -0600

    Time: 7 ms (82.03%), Space: 18 MB (39.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,28 +4,26 @@ class Solution:
         def backtrack(i, curr_sum, subset):
 
             if curr_sum == target:
-                res.append(subset.copy())
-                return 
+                res.append(subset[::])
+                return
 
             if curr_sum > target:
                 return
-                
+
             for j in range(i, len(candidates)):
-                
+
+                curr_sum += candidates[j]
                 subset.append(candidates[j])
-                curr_sum = curr_sum + candidates[j]
 
                 backtrack(j, curr_sum, subset)
 
+                curr_sum -= candidates[j]
                 subset.pop()
-                curr_sum = curr_sum - candidates[j]
 
-
-            return
+            return 
 
         res = []
         backtrack(0, 0, [])
         return res
 
-
-            
+                
