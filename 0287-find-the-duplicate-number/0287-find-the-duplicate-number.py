a4a2d8c4a8a094bee0f8f6a8b635f94cc9f02bba
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-11-29 09:35:09 -0600

    Time: 188 ms (5.11%), Space: 30.2 MB (32.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,30 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        """
-        Given nums := List[int] - n + 1 integers and each integer is in the range [1, n]
+        l, r = 0, len(nums) - 1
 
-        Only one repeated num in nums - R: this repeated integer
+        while l < r:
+            m = l + (r - l) // 2
 
-        nums = [1,3,4,2,2]
-        Approach 1: get the Counter of each value and return the key that its value is > 1
-        O(N) time, O(N) space
+            pivot = sum(1 for num in nums if num <= m)
 
-        Approach 2:
-        Use pointers 
+            if pivot <= m:
+                l = m + 1
 
-                 0  1  2  3 4
-        nums = [-1,-3,-4,-2,2]
-                            i
+            else:
+                r = m
 
-                idx = abs(num[i]) - 1 = 1 - 1 = 0
-                idx = abs(num[i]) - 1 = 3 - 1 = 2
-                idx = abs(-4) - 1 = 3
-                idx = abs(-2) - 1 = 1
-                idx = abs(2) - 1 = 1
+        return l
                 
-        """
-        for num in nums:
-            idx = abs(num) - 1
-            if nums[idx] < 0:
-                return abs(num)
-            nums[idx] *= -1 
