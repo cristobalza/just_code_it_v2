0883043efebb8bf6da9cb6030b6b73b6a214aa68
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-03 20:49:23 -0600

    Time: 3 ms (63.45%), Space: 18.8 MB (35.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,33 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
-        nums_hs = set(nums)
-        for num in range(len(nums) + 1):
-            if num not in nums_hs:
-                return num
+        """            
+                       0 1 2
+        Input: nums = [3,0,1]
+                       i
+
+        size = 3
+
+        i = 0
+        nums[i] = 3
+
+        size = 3 - 0 + 3 = 0
+
+        i = 2
+        nums[i] = 1
+
+        size = 0 - (0-1) = 1
+
+        i = 1
+        nums[i] = 0
+
+        size = 1 - (0 - 1) = 2
+
+        """
+
+        size = len(nums)
+
+        for i in range(len(nums)):
+            size += i - nums[i]
+
+        return size
         
