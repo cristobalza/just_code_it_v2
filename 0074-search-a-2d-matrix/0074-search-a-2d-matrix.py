0d865c95ee8d740441ce6e3352dc889a67296c65
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-04 12:37:30 -0600

    Time: 1 ms (6.31%), Space: 18.5 MB (5.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,31 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0])
+        # 1. binary search on ROWS -> log(ROWS) == log(m)
+        # 2. binary search on COLS -> log(COLS) == log(n)
+        # ==> log(m) + log(n) = log(m*n)
+        # space: O(1)
 
-        top, bottom = 0, m - 1
+        ROWS, COLS = len(matrix), len(matrix[0])
+        # 1. binary search on ROWS
+        u, d = 0, ROWS - 1
 
-        while top <= bottom:
-            row = (top + bottom) // 2
+        while u < d:
+            m = (u + d) // 2
 
-            if target < matrix[row][0]:
-                bottom = row - 1
-            elif target > matrix[row][-1]:
-                top = row + 1
-            else:
-                break
-
-        l, r = 0, n - 1
-        row = (top + bottom) // 2
-
-        while l <= r:
-            mid = (l + r) // 2
-            if matrix[row][mid] > target:
-                r -= 1
-            elif matrix[row][mid] < target:
-                l += 1
-            else:
+            if matrix[m][COLS - 1] == target:
                 return True
+            elif matrix[m][COLS - 1] < target:
+                u = m + 1
+            else:
+                d = m
 
-        return False
-
+        row_pivot = u
+        arr = matrix[row_pivot]
         
+        # 2. binary search on COLS
+        l, r = 0, COLS - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] == target:
+                return True
+            elif arr[m] < target:
+                l = m + 1
+            else:
+                r = m
+
+        return True if arr[l] == target else False
