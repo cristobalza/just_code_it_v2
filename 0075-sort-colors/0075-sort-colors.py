7bf124993c781269b7a3cda6ca0ca6b169d94bc9
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-05 12:41:25 -0600

    Time: 4 ms (5.8%), Space: 17.7 MB (53.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -28,15 +28,13 @@ class Solution:
 
         """
 
-        count_0, count_1, count_2 = 0, 0 ,0
+        count_0, count_1 = 0, 0
 
         for num in nums:
             if num == 0:
                 count_0 += 1
             elif num == 1:
                 count_1 += 1
-            else:
-                count_2 += 1
 
         for i in range(len(nums)):
             if 0 <= i < count_0:
