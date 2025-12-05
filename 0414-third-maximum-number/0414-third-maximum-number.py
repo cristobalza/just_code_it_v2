8edf004bedce7eff06a4ae9b8ff93521a309643b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-05 11:34:23 -0600

    Time: 5 ms (6.18%), Space: 18.6 MB (65.55%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,20 +1,42 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
 
-        maxheap = []
+        # maxheap = []
 
-        for num in set(nums):
-            heapq.heappush(maxheap, (-num))
+        # for num in set(nums):
+        #     heapq.heappush(maxheap, (-num))
 
-        k = 3
+        # k = 3
 
-        res = 0
-        while k > 0:
-            if maxheap:
-                res = -1 * heapq.heappop(maxheap)
-            else:
-                return max(nums)
-            k -= 1
+        # res = 0
+        # while k > 0:
+        #     if maxheap:
+        #         res = -1 * heapq.heappop(maxheap)
+        #     else:
+        #         return max(nums)
+        #     k -= 1
 
-        return res 
+        # return res 
         
+
+
+        minheap = [] # at least 3 values -> O(1) space
+        hs = set() # at least 3 values -> O(1) space
+
+        for i in range(len(nums)):
+            if nums[i] in hs:
+                continue
+            
+            hs.add(nums[i])
+            heapq.heappush(minheap, nums[i])
+
+            if len(minheap) > 3:
+                val = heapq.heappop(minheap)
+                hs.remove(val)
+
+        if len(minheap) == 3:
+            return minheap[0]
+
+        else:
+            return max(minheap)
+                
