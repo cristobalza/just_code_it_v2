9d15dcf6db5411cd9cf2692d4bebd57097ec3f70
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-09 13:12:39 -0600

    Time: 11 ms (71.05%), Space: 19.3 MB (38.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # two pointers
         l, r = 0, len(height) - 1
         max_l, max_r = height[l], height[r]
 
         res = 0
 
         while l < r:
-            if height[l] < height[r]:
+            if height[l] > height[r]:
+                res += max_r - height[r]
+                r -= 1
+                max_r = max(max_r, height[r])      
+            else:
                 res += max_l - height[l]
                 l += 1
                 max_l = max(max_l, height[l])
-
-            else:
-                res += max_r - height[r]
-                r -= 1
-                max_r = max(max_r, height[r])
 
         return res
         
