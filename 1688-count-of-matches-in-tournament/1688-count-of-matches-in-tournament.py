f8622dfd9c95ff64d8d4f3b97c976a4be264c3fc
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-09 20:57:50 -0600

    Time: 28 ms (96.78%), Space: 17.7 MB (46.94%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -3,10 +3,10 @@ class Solution:
         res = 0
         while n > 1:
             if n % 2 == 0:
-                res += n // 2
-                n = n // 2
+                res += n // 2 # matches played
+                n = n // 2 # team advanced
             else:
-                res += (n - 1) // 2
-                n = (n - 1) // 2 + 1
+                res += (n - 1) // 2 # matches played
+                n = (n - 1) // 2 + 1 # team advanced
         return res
         
