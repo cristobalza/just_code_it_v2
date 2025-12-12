65f94ae19c1fef33727062e68be1a999766d255d
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-11 19:18:29 -0600

    Time: 7 ms (46.36%), Space: 19.9 MB (10.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,46 +1,18 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        """
-        Given numCourses and prerequisites, return true if can finish the coursers
 
-        adj. List
-        0 []
-        1 [0]
+        def dfs(course, cycle):
 
-
-        check for 0
-        {0, 1}
-        -> [1]
-            X
-            check for 1 and 
-            True
-        {1, 0}
-
-        same size of numCourses
-
-        backtracing approach
-
-        base case: if it is in the cycle (False) or has been visited (True)
-        mark candidate: add to the cycle check
-        explore: iterate through the prerequisites
-        remove candidate: rm from cycle and add to the visited
-        
-        return True
-        """
-
-        def backtrack(course, cycle):
+            if course in visited:
+                return True
 
             if course in cycle:
                 return False
 
-            if course in visited:
-                return True
-            
             cycle.add(course)
 
-            for prereq in graph[course]:
-                if not backtrack(prereq, cycle):
+            for prereq in adj[course]:
+                if not dfs(prereq, cycle):
                     return False
 
             cycle.remove(course)
@@ -48,14 +20,18 @@ class Solution:
 
             return True
 
-        graph = {course: [] for course in range(numCourses)}
+
+        adj = {course: [] for course in range(numCourses)}
+        # Directed graph
         for a, b in prerequisites:
-            graph[a].append(b)
+            adj[a].append(b)
 
         visited = set()
-
         for course in range(numCourses):
-            if not backtrack(course, set()):
-                return False
+            if course in visited:
+                continue
+            else:
+                if not dfs(course, set()):
+                    return False
 
         return True if len(visited) == numCourses else False
