e75c1aeb9f2a45d0d514aa64893f26b5903cc613
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-22 13:32:44 -0600

    Time: 59 ms (6.6%), Space: 18.6 MB (74%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,25 +1,42 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         """
+        Given
+            nums: list of strings that contain digits
+            k; integer that set the number of strings possible to remove
+
         num = "1432219", k = 3
                     i
 
-        num = 2
-        stack[-1]> num 2 > 1
-        k = 0
-        stack [1, 2, 1, 9]
+        montonic increasing stack
 
+        stack = [1, 2, 1, 9]
+        k = 0
+        num = 2
+
+        while stack and stack[-1] >= num and k > 0:
+            stack.pop()
+            k -= 1
+
+        stack.append(num)
+               
         """
-        # Monotically decreasing
 
         stack = []
 
-        for charnum in num:
-            while stack and stack[-1] > charnum and k > 0:
+        for i, ch_num in enumerate(num):
+            
+            while stack and int(stack[-1]) > int(ch_num) and k > 0:
                 stack.pop()
                 k -= 1
-            if stack or charnum != "0":
-                stack.append(charnum)
+            
+            stack.append(ch_num)
 
-        stack = stack[:-k] if k else stack
-        return "".join(stack).lstrip('0') if stack  else '0'
+        if k > 0:
+            stack = stack[:-k]  # Remove last k elements
+
+        res = "".join(stack).lstrip("0")
+        return res if res != "" else "0"
+
+
+        
