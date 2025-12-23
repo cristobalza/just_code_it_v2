daee88260bf38caf8bea063d055876ee33b56b07
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-23 08:53:05 -0600

    Time: 56 ms (26.01%), Space: 29.1 MB (98.65%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,28 +1,32 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         """
+        Given
+            heights: array int
+        
+        A builiding has an ocean view, if all of the buildings right to it have smaller height
 
-        heights = [4,2,3,1]
-                         i
+        Return
+            list of indices of buildings that actually have a ocean view, sorted increasing
 
-        stack = [0, 2, 3]
+        Example 1:
+        Input: heights = [4,2,3,1]
+                                i
 
-        monotonally decreasing as
+        [0, 2, 3]
+
+        monotonically increasing
 
         """
 
-        stack = [] # store indexes
+        stack = []
 
         for i, h in enumerate(heights):
 
-            # pop values that are <= current h (not useful as curr h is taller than last h)
             while stack and heights[stack[-1]] <= h:
                 stack.pop()
 
             stack.append(i)
 
         return stack
-
-
-
         
