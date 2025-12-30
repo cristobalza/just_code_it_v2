2154d83eb4d67d7d43fb3964a3470fccee0e6fde
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2025-12-30 08:53:24 -0600

    Time: 79 ms (37.95%), Space: 20.6 MB (67.71%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,31 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
+
+        res = 0
+
+        i, n = 0, len(s)
+
         stack = []
-        num = 0
-        operation = '+'
-        
+
+        num, operation = 0, "+"
+
         for i, ch in enumerate(s):
             if ch.isdigit():
                 num = num * 10 + int(ch)
-            
-            # Process when we hit an operator or end of string
-            if ch in ['+', '-', '*', '/'] or i == len(s) - 1:
-                if operation == '+':
+
+            if ch in ['+', '-', '*', '/'] or i == len(s) - 1: # check operations or end of string
+                if operation == "+":
                     stack.append(num)
-                elif operation == '-':
+                if operation == "-":
                     stack.append(-num)
-                elif operation == '*':
-                    stack.append(stack.pop() * num)
-                elif operation == '/':
-                    stack.append(int(stack.pop() / num))
-                
-                if i < len(s) - 1:
-                    operation = ch
-                num = 0 # reset the num
-        
+                if operation == "*":
+                    last_num = stack.pop()
+                    stack.append(last_num * num)
+                if operation == "/":
+                    last_num = stack.pop()
+                    stack.append(int(last_num / num))
+
+                operation = ch
+                num = 0
+
         res = 0
         for val in stack:
             res += val
-        
         return res
