902093784acd848cc05c1f58a9745d124e1a749b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2026-01-01 06:12:45 -0600

    Time: 9024 ms (5.07%), Space: 31.9 MB (53.19%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,38 +1,32 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        """
-Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-               
 
-        deque = nums[: k+1] = [1, 3, -1] -> get_max -> 3
-
-        then popleft and append new value
-
-        res = [3]
-
-        """
-
-        # Monotonically decreasing queue
-    
         q = collections.deque()
+        max_val = float("-inf")
+        hmap = {}
+        for i in range(k):
+            max_val = max(max_val, nums[i])
+            q.append(nums[i])
+            hmap[nums[i]] = 1 + hmap.get(nums[i], 0)
 
         res = []
+        res.append(max_val)
 
-        l = 0
+        for i in range(k, len(nums)):
+            pop_val = q.popleft()
+            hmap[pop_val] -= 1
+            if hmap[pop_val] == 0:
+                del hmap[pop_val]
+            
+            if max_val == pop_val:
+                max_val = max(hmap.keys()) if hmap else float("-inf")
 
-        for r in range(len(nums)):
+            q.append(nums[i])
+            hmap[nums[i]] = 1 + hmap.get(nums[i], 0)
 
-            while q and nums[q[-1]] < nums[r]:
-                q.pop()
+            max_val = max(max_val, nums[i])
 
-            q.append(r)
-
-            if l > q[0]:
-                q.popleft()
-                
-
-            if (r + 1) >= k:
-                res.append(nums[q[0]])
-                l += 1
+            res.append(max_val)
 
         return res
+         
